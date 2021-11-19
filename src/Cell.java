0f28c912c160578054e09ab1a4541d87b4fa52ba
Administrator <tl1071@bu.edu>
2021-11-18 23:07:58 -0500

    cell change

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * abstract class for each cell, inaccessible,
  * marketCell and common cell needs to extend this class
@@ -7,11 +9,44 @@ public abstract class Cell
     private int row;
     private int col;
     private String value;
+    private String[][] pos;
 
     public Cell(int row, int col, String value) {
         this.row = row;
         this.col = col;
         this.value = value;
+        pos = new String[3][5];
+        initCell();
+    }
+
+    private void initCell(){
+        for(int r = 0; r < pos.length; r++ ){
+            for(int c = 0; c < pos[0].length; c++){
+                //even row
+                if( r % 2 == 0){
+                    //even col
+                    if( c % 2 == 0 ){
+                        pos[r][c] = this.value;
+                    }else{
+                        pos[r][c] = " - ";
+                    }
+                }else{
+                    if(c == 0 || c == pos[0].length - 1){
+                        pos[r][c] = "|";
+                    }else if( c == pos[0].length / 2){
+                        pos[r][c] = " ";
+                    }else{
+                        pos[r][c] = "   ";
+                    }
+                }
+            }
+        }
+    }
+
+    public void printCell(){
+        for (String[] po : this.pos) {
+            System.out.println(Arrays.toString(po));
+        }
     }
 
     //getter and setter
